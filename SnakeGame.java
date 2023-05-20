28c3ca9633531e0609292358a79e0b71fa7a75e0
Sangisetti Jayanth <132438843+S-jayanth@users.noreply.github.com>
2023-05-20 10:54:09 +0530

    Add files via upload

---
+++ SnakeGame.java
@@ -0,0 +1,11 @@
+package org.game.snakegameing;
+
+public class SnakeGame {
+	public static void main(String[] args) {
+		// creating an instance of GameFrame class
+		new GameFrame();
+		
+		
+	}
+
+}
